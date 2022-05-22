1e9c9b1236bffe66028e9f0a42e38323eca0b70b
Jagitro <info@jagitro.com>
2022-05-23 01:10:39 +0530

    Question first solved, OPtimize later

--- DSA Practice/Program.cs
+++ DSA Practice/Program.cs
@@ -4,10 +4,46 @@
 {
     class Program
     {
-        // Qu. 
+        // Qu.1 max And min of array 
+        public static int dataVal(int[] inputArray, int condition=0)
+        {
+            int data = inputArray[0];
+            
+            switch (condition)
+            {
+                case 0:
+                    for (int i = 1; i < inputArray.Length; i++)
+                    {
+                        if (data < inputArray[i])
+                        {
+                            data = inputArray[i];
+                        }
+                    }
+                    break;
+                   
+                case 1:
+                    for (int i = 1; i < inputArray.Length; i++)
+                    {
+                        if (data > inputArray[i])
+                        {
+                            data = inputArray[i];
+                        }
+                    }
+                    break;
+                default:
+                    break;
+
+            }
+            return data;
+           
+        }
+
         public static void Main(String[] Args)
         {
-            Console.WriteLine("Inside Code");
+            int[] inputArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+            int maxVal = dataVal(inputArray);
+            int minVal = dataVal(inputArray,1);
+            Console.WriteLine("Max Value in array is {0} and min value is {1}", maxVal,minVal);
         }
     }
 }
