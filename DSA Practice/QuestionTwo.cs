19674561f9f393deca12989be844aa00da1bb034
Jagitro <info@jagitro.com>
2022-05-23 20:37:27 +0530

    Space complexity resolved

--- DSA Practice/QuestionTwo.cs
+++ DSA Practice/QuestionTwo.cs
@@ -14,23 +14,44 @@ namespace DSA_Practice
             //Reersed Array
             int[] reversedArray = new int[array.Length];
             int index = 0;
-            for(int i=array.Length-1;i>=0;i--)
+            for (int i = array.Length - 1; i >= 0; i--)
             {
 
                 reversedArray[index] = array[i];
                 index += 1;
-                
 
             }
             return reversedArray;
         }
-        //public static void Main(string[] args)
-        //{
-        //    int[] inputArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
-        //    foreach (int item in ReverseArray(inputArray))
-        //    {
-        //        Console.WriteLine(item);
-        //    }
-        //}
+        public static int[] ReverseSameArray(int[] Array)
+        {
+            //Revese within same array
+            int start = 0;
+            int end = Array.Length - 1;
+            int middle = end / 2;
+
+            for (int i = start; i <= middle; i++)
+            {
+                int temp = Array[i];
+                Array[i] = Array[end];
+                Array[end] = temp;
+                end--;
+            }
+
+
+            return Array;
+        }
+
+
+
+        public static void Main(string[] args)
+        {
+            int[] inputArray = { 0, 1 };
+            ReverseSameArray(inputArray);
+            foreach (int item in ReverseArray(inputArray))
+            {
+                Console.WriteLine(item);
+            }
+        }
     }
 }
